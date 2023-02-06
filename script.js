c0b797ab86b3511ff3f9d8cd747dfeb8461d8cc6
Benjamin <benjamin.lewin@yahoo.com>
2023-02-06 09:59:30 -0500

    font family selected

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
- //Goal -> pull a Bible Verse with a searchbar
-//render it on screen with JS DOM
-
-//make HTML, CSS, and JS files
-//connect (scaffold) our files together 
-
-//find a decent API √
-//make an API call with a url 
-
-
-//set up our API call in JS 
-//log our data 
-
-//send it and render it through HTML
-
-//make text input and search button 
-//connect all together 
-
-//style HTML with background (maybe scroll?) 
-//
-
 let button = document.querySelector("#searchButton")
 
 async function getData(event) {
